dc5a50c726987c3ac4d09a089276437d7058fbe0
zhym <tjzhym@hotmail.com>
2021-08-04 14:06:47 -0400

    add JC005 cpp file

---
+++ Algorithms/JC/JC005 用两个栈实现队列/JC005.cpp
@@ -0,0 +1,40 @@
+// Problem :    https://www.nowcoder.com/activity/oj
+// Solution:    https://github.com/tjzhym/Nowcoder
+// Author :     zhym (tjzhym)
+// Date   :     2021-8-4
+
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+class Solution
+{
+public:
+    void push(int node) {
+        stack1.push(node);
+    }
+
+    int pop() {
+        if (stack2.empty()) {
+            while (!stack1.empty()) {
+                stack2.push(stack1.top());
+                stack1.pop();
+            }
+        }
+        int output = stack2.top();
+        stack2.pop();
+        return output;
+    }
+
+private:
+    stack<int> stack1;
+    stack<int> stack2;
+};
+
+int main()
+{
+    Solution Solution;
+
+}
