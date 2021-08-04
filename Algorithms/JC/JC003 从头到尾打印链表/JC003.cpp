abe79aa007af83594b61572a20e4a5f35e3125f2
zhym <tjzhym@hotmail.com>
2021-08-04 13:44:28 -0400

    add JC003 cpp file

---
+++ Algorithms/JC/JC003 从头到尾打印链表/JC003.cpp
@@ -0,0 +1,48 @@
+// Problem :    https://www.nowcoder.com/activity/oj
+// Solution:    https://github.com/tjzhym/Nowcoder
+// Author :     zhym (tjzhym)
+// Date   :     2021-8-4
+
+#include <vector>
+#include <iostream>
+struct ListNode;
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+    ListNode(int x) :
+          val(x), next(NULL) {
+    }
+};
+
+class Solution {
+public:
+    vector<int> printListFromTailToHead(ListNode* head) {
+        vector<int> outVector;
+        ListNode* reversedListNode = NULL;
+        ListNode* pTemp = NULL;
+        while (head != NULL) {
+            pTemp = head->next;
+            head->next = reversedListNode;
+            reversedListNode = head;
+            head = pTemp;
+        }
+        while (reversedListNode != NULL) {
+            outVector.push_back(reversedListNode->val);
+            reversedListNode = reversedListNode->next;
+        }
+        return outVector;
+
+    }
+};
+
+int main()
+{
+    Solution Solution;
+    
+    Solution.printListFromTailToHead();
+    cout << output << endl;
+
+}
