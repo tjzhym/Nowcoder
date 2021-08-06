1c876141447510e15c8d2b75017be1cd176f2d31
zhym <tjzhym@hotmail.com>
2021-08-06 03:15:13 -0400

    add JC007 cpp file

---
+++ Algorithms/JC/JC007 斐波那契数列/JC007.cpp
@@ -0,0 +1,32 @@
+// Problem :    https://www.nowcoder.com/activity/oj
+// Solution:    https://github.com/tjzhym/Nowcoder
+// Author :     zhym (tjzhym)
+// Date   :     2021-8-4
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int Fibonacci(int n) {
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        else
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+};
+
+int main()
+{
+    Solution Solution;
+    int number = 0;
+    cout << "Please input the n-th index:" << endl;
+    cin >> number;
+    int FNumber = Solution.Fibonacci(number);
+    cout << FNumber << endl;
+}
