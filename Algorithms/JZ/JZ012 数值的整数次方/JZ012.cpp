ae0516793e22e59e489699acc79754cad8a56991
zhym <tjzhym@hotmail.com>
2021-08-06 04:28:44 -0400

    add JZ012 cpp file

---
+++ Algorithms/JZ/JZ012 数值的整数次方/JZ012.cpp
@@ -0,0 +1,40 @@
+// Problem :    https://www.nowcoder.com/ta/coding-interviews
+// Solution:    https://github.com/tjzhym/Nowcoder
+// Author :     zhym (tjzhym)
+// Date   :     2021-8-6
+
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    double Power(double base, int exponent) {
+        if (exponent < 0) {
+            base = 1 / base;
+            exponent = -exponent;
+        }
+        if (base == 0.0)
+            return 0.0;
+        else if (exponent == 0)
+            return 1.0;
+
+        while (exponent != 0) {
+            exponent--;
+            return Power(base, exponent) * base;
+        }
+
+        return -1;
+    }
+};
+
+int main()
+{
+    Solution Solution;
+    int base = 0, exponent = 0;
+    cout << "Please input base and integer exponent:" << endl;
+    cin >> base >> exponent;
+    int powerResult = Solution.Power(base, exponent);
+    cout << powerResult << endl;
+}
